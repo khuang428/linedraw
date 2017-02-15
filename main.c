816b0eef412e4c3e9c1f7e59c20927586c74c16d
Karen Huang <khuang428@gmail.com>
2017-02-15 11:02:46 -0500

    shapes and stuff

--- main.c
+++ main.c
@@ -6,6 +6,13 @@
 #include "display.h"
 #include "draw.h"
 
+void square(int x, int y, screen s, color c){
+  draw_line(x,x,x,y,s,c);
+  draw_line(x,x,y,x,s,c);
+  draw_line(y,y,y,x,s,c);
+  draw_line(y,y,x,y,s,c);
+}
+
 int main() {
 
   screen s;
@@ -17,7 +24,8 @@ int main() {
   c.blue = 0;
   
   clear_screen(s);
-
+  
+  /*
   draw_line(0,0,500,100,s,c);
   draw_line(0,0,500,500,s,c);
   draw_line(0,250,500,250,s,c);
@@ -26,6 +34,12 @@ int main() {
   draw_line(0,500,500,400,s,c);
   draw_line(500,0,0,500,s,c);
   draw_line(100,0,0,500,s,c);
+  */
+
+  square(167,334,s,c);
+  square(125,375,s,c);
+  draw_line(0,0,500,500,s,c);
+  draw_line(500,0,0,500,s,c);
   
   display(s);
   save_extension(s, "lines.png");
